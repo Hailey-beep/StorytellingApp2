6a1ea4ec2a06eb7b27eb8c7c89494e21aa1c1393
Hailey <72172315+Hailey-beep@users.noreply.github.com>
2022-08-21 20:05:46 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyB8x9NbLTkSWANGHytPkRsczrepTzxIMm4",
+  authDomain: "storytelling-2bcc2.firebaseapp.com",
+  databaseURL: "https://storytelling-2bcc2-default-rtdb.firebaseio.com",
+  projectId: "storytelling-2bcc2",
+  storageBucket: "storytelling-2bcc2.appspot.com",
+  messagingSenderId: "716486969363",
+  };
